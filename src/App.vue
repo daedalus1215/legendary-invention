2e6adceaf6fec42982982b45a8d691702b9f5951
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-07-08 20:36:38 -0400

    carry over from refactoring-eureka

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app" class="row d-flex justify-content-center">
+    <div class="col-md-6 mt-3">
+      <div class="card">
+        <div class="card-header">Newsletter</div>
+        <div class="card-body">
+          <form @submit.prevent="submit">
+            <div class="form-group">
+              <EmojiInput v-model="emoji" :options="{ position: bottom }" />
+            </div>
+            <div class="mb-2">
+              <EmailInput v-model:email="newsletterEmail" />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import EmailInput from './components/EmailInput.vue';
+import EmojiInput from './components/EmojiInput.vue';
+
+export default {
+  name: "App",
+  components: {
+    EmailInput,
+    EmojiInput
+  },
+  data() {
+    return {
+      newsletterName: "",
+      newsletterEmail: "",
+      emoji: '',
+    };
+  },
+  methods: {
+    submit() {
+      console.log("Newletter Submitted", {
+        name: this.newsletterName,
+        email: this.newsletterEmail,
+      });
+    },
+  },
+};
+</script>
+
+<style src="bootstrap/dist/css/bootstrap.css"></style>
